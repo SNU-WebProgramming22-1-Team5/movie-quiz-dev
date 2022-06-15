303fd9a74c69b22b986c615b8dbfb46b4c576bbd
VitaminK1 <33277896+VitaminK1@users.noreply.github.com>
2022-06-16 00:58:05 +0900

    Added more comments and functions related to firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
-import { getFirestore, doc, setDoc, getDoc } from 'firebase/firestore';
+import { getFirestore, collection, doc, getDoc, updateDoc, arrayUnion, setDoc } from 'firebase/firestore';
 import { getAuth, signInWithPopup, GoogleAuthProvider, signInAnonymously, updateProfile} from "firebase/auth";
 import { browserLocalPersistence, setPersistence } from "firebase/auth";
 
@@ -30,7 +30,7 @@ export const loginGoogle = () => {
     return signInWithPopup(auth, provider);
 }
 
-//for anonymous login (guest play)
+//Changeable - for anonymous login (guest play)
 export const loginGuest = () => {
     return signInAnonymously(auth)
 }
@@ -49,19 +49,57 @@ export const persistenceSet = () => {
     setPersistence(auth, browserLocalPersistence).catch(error => console.log(error));
 }
 
-// Currently testing - for writing data on firestore.
-export const add_history = async (user_id, name, score, date) => {
-    //const fileName = String(Date.parse(date));
-    const docSnap = await getDoc(doc(db,"scoreHistory",user_id));
-    if (!docSnap.exists()) {
-        await setDoc(doc(db,"scoreHistory", user_id), {
-            name: name,
-            score: score,
-            date: date,
-        })
+//for writing game result score on firestore.
+export const addScore = async (user_id, name, score) => {
+    const docRef = doc(db,"userData",user_id)
+    const docSnap = await getDoc(docRef);
+    if (docSnap.exists()){
+        await updateDoc(docRef,{
+            scores: arrayUnion(score)
+        });
     } else {
-        console.log('LOL');
+        console.log('No Document Record');
+    }
+
+}
+
+// if there's no info on UserData with user's id, add new document with blank score history and email
+export const addUser = async (user_id,email,score=0) => {
+    const docSnap = await getDoc(doc(db, "userData", user_id));
+    if (!docSnap.exists()) {
+        const data = {
+            email : email,
+            scores : []
+        }
+        await setDoc(doc(db, "userData", user_id), data).catch(err => console.log(err))
     }
 }
+
+//for adding title data of a movie on movieData DB with random-gen id-named document
+export const addMovie = async (title1, title2, title3) => {
+    const docRef = doc(collection(db,"movieData"));
+    const data = {
+        title1 : title1,
+        title2 : title2,
+        title3 : title3
+    }
+    await setDoc(docRef, data);
+}
+
+//for getting top 10 scores and username info from db. (scoreboard info)
+export const getScoreboard = async () => {
+
+}
+
+//for getting rank of game result. guest doesn't get rank.
+export const getRank = () => {
+
+}
+
+//for getting 10 movies data for quiz.
+export const getQuiz = () => {
+
+}
 export { db, auth };
+
 
